933bc10efcc23f17df9847e5d2055c35936407cc
Adebayo Temitope <tsadebayo@myseneca.ca>
2023-10-09 16:12:00 -0400

    passing data into views

--- LandmarkNavigation/Views/CircleImage.swift
+++ LandmarkNavigation/Views/CircleImage.swift
@@ -8,17 +8,19 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
+    
     var body: some View {
-        Image("turtlerock")
+        image
             .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
             .overlay(
                 Circle()
                     .stroke(.white, lineWidth: 4)
             )
-            .shadow(radius: 4)
+            .shadow(radius: 7)
     }
 }
 
 #Preview {
-    CircleImage()
+    CircleImage(image: Image("turtlerock"))
 }
