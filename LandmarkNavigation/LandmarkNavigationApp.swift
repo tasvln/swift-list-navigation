23c2f1d9906f429a06524b50d3c4b431eba983e3
Adebayo Temitope <tsadebayo@myseneca.ca>
2023-10-09 12:39:54 -0400

    Initial Commit

---
+++ LandmarkNavigation/LandmarkNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkNavigationApp.swift
+//  LandmarkNavigation
+//
+//  Created by Temitope Adebayo on 2023-10-09.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
