dc27da0d5ad430bb36cf82018f100ee8e1705847
Adebayo Temitope <tsadebayo@myseneca.ca>
2023-10-09 18:02:45 -0400

    complete tutorial

---
+++ LandmarkNavigation/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,26 @@
+//
+//  FavoriteButton.swift
+//  LandmarkNavigation
+//
+//  Created by Temitope Adebayo on 2023-10-09.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
